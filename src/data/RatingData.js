d4c0cd7815992266894ec646072de5e920cd4731
hermy1 <49494077+hermy1@users.noreply.github.com>
2022-08-10 20:25:23 -0400

    changes minor

--- src/data/RatingData.js
+++ src/data/RatingData.js
@@ -1,6 +1,7 @@
 const RateData = [
   { id: 1, rate: 4, comment: "I enjoyed this experience. Thanks" },
   { id: 2, rate: 10, comment: "The best class I've taken so far. Thanks" },
+  { id: 3, rate: 15, comment: "This is my rating and i'm not changing it." },
 ];
 
 export default RateData
